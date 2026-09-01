884e40d18e9d4e19741fea50ca71ceb9e6d1546a
Anup Pandey <ansh47822@gmail.com>
2026-09-01 12:38:55 +0530

    Prepare backend for Aiven MySQL

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -5,11 +5,19 @@ export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "mysql",
+
   dbCredentials: {
     host: process.env.DB_HOST!,
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER!,
     password: process.env.DB_PASSWORD!,
     database: process.env.DB_NAME!,
+
+    ssl:
+      process.env.DB_SSL === "true"
+        ? {
+            rejectUnauthorized: false,
+          }
+        : undefined,
   },
 });
